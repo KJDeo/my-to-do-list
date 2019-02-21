ceeeb6fe8f762affe7088d93f63cfff424714dd5
KJ Deo <k.deo@msn.com>
2019-02-21 15:12:51 -0500

    Code Java

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let tasks = [
+    {name:"Prepare for IWB Charrette", available:true},
+    {name:"Buy gift for siblings birthday", available:true}, 
+    {name:"Mail package to a friend", available:false}
+    {name:"Work on title sequence", available:true},
+    {name:"Plan marketing for the MTF Hackathon", available:false}, 
+  ];
