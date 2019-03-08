85e6a83a1c8228b4eae41e215114df1a022565a2
KJ Deo <k.deo@msn.com>
2019-03-08 15:15:57 -0500

    Updating to do list with java

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-let tasks = [
-    {name:"Prepare for IWB Charrette", available:true},
-    {name:"Buy gift for siblings birthday", available:true}, 
-    {name:"Mail package to a friend", available:false}
-    {name:"Work on title sequence", available:true},
-    {name:"Plan marketing for the MTF Hackathon", available:false}, 
-  ];
+const school = document.getElementById(`school`);
+const personal = document.getElementById(`personal`);
+const completed = document.getElementById(`completed`);
+
+const todoList = [
+  { name: `Prepare for IWB Charrette`, cat: 'school'},
+  { name: `Buy gift for siblings birthday`, cat: 'personal'},
+  { name: `Mail package to friend`, cat: 'personal'},
+  { name: `Work on title sequence`, cat: 'school'},
+  { name: `Finish Javascript for To-Do list`, cat: 'completed'},
+  { name: `Plan marketing for the MTF Hackathon`, cat: 'school'},
+  { name: `Dogsit for roommate`, cat: 'completed'},
+];
+
+school.innerHTML = todoList
+                    .filter(item => item.cat == `school`)
+                    .map(item => `<li>I need to ${item.name}.</li>`)
+                    .join('');
+
+personal.innerHTML = todoList
+                    .filter(item => item.cat == `personal`)
+                    .map(item => `<li>I need to ${item.name}.</li>`)
+                    .join('');
+
+completed.innerHTML = todoList
+                    .filter(item => item.cat == `completed`)
+                    .map(item => `<li>I need to ${item.name}.</li>`)
+                    .join('');
